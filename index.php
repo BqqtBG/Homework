07bccb56e1c2b692f4d62b1c74440181914dcd6e
BqqtBG <bobi514@abv.bg>
2020-12-04 22:01:03 +0200

    Test

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+   <head>
+      <meta charset="UTF-8">
+      <title>Store</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <link rel="stylesheet" href="assets/css/main.css"/>
+   </head>
+   <body>
+      <div class="topnav">
+          <a style="float: right" class="active" href="#">Log In</a>
+        <a style="float: right" class="active" href="#">Sign Up</a>
+      </div>
+      <h1>aefasdfasfa</h1>
+      <h2>aasfasfasfs</h2>
+      <h2>aasfasfasfs</h2>
+      <h2>aasfasfasfs</h2>
+      <h2>aasfasfasfs</h2>
+      <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#news">Store</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About Us</a>
+      </div>
+
+   </body>
+</html>
